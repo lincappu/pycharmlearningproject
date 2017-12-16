29b65fbce2b6e0d68cf9fbf4c92d3686f2fe0670
lincappu <lincappu@163.com>
2017-12-16 09:45:26 +0800

    数据结构+字符编码+第二次作业

--- day1/基础内容.py
+++ day1/基础内容.py
@@ -1,16 +1,16 @@
 # !/usr/bin/env python3
 # -*- coding:utf-8 -*-
-
-#变量
-#a=100087
-#print(id(a),type(a),a)   驼峰体和下划线 （推荐使用下划线）
-#常量的意义---将变量名全部携程大写的，提示为常量，但是其实还是可以修改的
-
-#程序交互：
-#oython3中，所有input都是string类型的，
-#python2中输入什么类型就是什么类型，raw_input等于python3中的input
-#注意要想要 int 类型的使用 int(input(''))
-
+#
+# 变量
+# a=100087
+# print(id(a),type(a),a)   驼峰体和下划线 （推荐使用下划线）
+# 常量的意义---将变量名全部携程大写的，提示为常量，但是其实还是可以修改的
+#
+# 程序交互：
+# oython3中，所有input都是string类型的，
+# python2中输入什么类型就是什么类型，raw_input等于python3中的input
+# 注意要想要 int 类型的使用 int(input(''))
+#
 # x = 1 - 2j
 # print(x.imag)
 # print(x.real)
@@ -36,11 +36,181 @@
 # print(name+age)
 #
 # print(name*5)
+#
+# 数据类型：
+#
+# x = 2.3
+# print(id(x),type(x),x)
+#
+# ==和 is  判断的类型：  is 是同一性运算符，判断两个变量id 是不是一样的。
+# 有个区别 ：
+# 当变量小于256时，两个变量的  x is y 是 true，大于256就不成立。pycharm 这个做了处理显示不出来的。
+# 当 x=123 y=x 这种是，x 永远等于 y，
+# 这两种区别是 python 结构限制的，申请了两块地址空间，所以 id 不可能相等。
+# 这种类型永远是整型和字符串才有的特性。
+# print(x is y)
+# print(id(x),id(y))
+# 长整型，python3中没有长整型， python2
+
+
+########字符换######
+# 字符串 :  顾头不顾尾
+# 内置函数：
+# x = 'helloworld     '
+# 按索引取值
+# print(x[1])
+# 切片,步长：
+# print(x[1::2])
+# print(x[5:1:-1])  # 逆向取值，
+# 长度：只有字符串有，整型没有这个属性。
+# in  判断
+# print('he' in x)
+# 移除空白strip,默认是两端的空格，
+# msg = '*****fs****ff****'
+# print(msg.strip('*'))
+# print(msg.lstrip('*'))
+# print(msg.rstrip('*'))
+# 切分操作，按特定的字符，
+# msg = 'root:x:0:0:/root'
+# print(msg.split(':')[0])
+# print(msg.split(':', 1))  # 1： 切分一次
+# name = 'C:/a/b/c/d.txt'
+# print(name.split('/',1))
+# print(name.split('/')[0])
+# print(name.split('/')[-1])
+# print(name.rsplit('/', 1)[0])
+#
+# msg = 'nihao'
+# print(msg.lower())
+# print(msg.upper())
+#
+# msg = 'abcdefg'
+# print(msg.startswith('ab'))
+# print(msg.endswith('fg'))
+#
+# msg = 'zhe  shi  wo  de aaa bbb cc  '
+# print(msg.replace('a', 'A', 1).strip())
+
+# format
+# print('my name is {0} my age is {1}'.format('egon', 18))
+# print('my name is {name} my age is {age}'.format(age='18', name='egon'))
+#
+# msg = 'li xin ni hao'
+# print(msg.find('ni'))  # 从左向右找，找到后返回第一个字符的索引
+# print(msg.find('fsdf'))
+# print(msg.index('ni'))  # 如果没有 则直接报错。
+#
+# msg = 'hello world'
+# print(msg.count('l', 0, 3))  # 返回的是个数
+#
+# msg = 'a:a:b:c:d:e'
+# l = msg.split(':')
+# l.append('f')  #默认是添加到最后
+# print(l.count('a'))
+# print(l)
+# print('/'.join(l))
+# print('#'.join(l))
+# tag = '#'
+# print(tag.join(['a', 'b', 'c']))
+
+# age=input('your age: ')
+# print(age.isdigit()) #只能判断 bytes 和 unicode类型，，判断"字符是否为数字" ##重要理解
+
+
+# find  rfind  index  rindex  count
+# name = 'egon say hello'
+# print(name.find('s', 1, 6))
+# print(name.index('s', 1, 6))
+# print(name.rfind('g', 1, 6))
+# print(name.count('e', 0, 13))
+
+# center ljust rjust zfill
+# msg = 'egon'
+# print(msg.center(18,'#'))
+# print(msg.ljust(18,'#'))
+# print(msg.rjust(18,'#'))
+# print(msg.zfill(18)) # 用0 填充
+
+
+# expangtable  制表符的个数
+# msg = 'abc\tdefs'
+# print(msg)
+# print(msg.expandtabs(5))
+
+
+# capitalize   swapcase  titile
+# msg = 'egon say Ello'
+# print(msg.capitalize())  #首字母大写
+# print(msg.lower()) #全部变小写
+# print(msg.upper()) #全部变大写
+# print(msg.title()) #每个单词的首字母大写
+# print(msg.swapcase()) #大小写翻转
+#
+# is  系列
+# msg='Avbcd'
+# print(msg.islower())
+# print(msg.isupper())
+# print(msg.istitle())
 
 
 
-#列表   取数据时要提前知道有几项，不方便。
-# stu=['egon','alex','wupieqi']
+# msg = 'abc'
+# print(msg.isalpha())   # 字符串是否由字母组成的
+# print(msg.isalnum()) #字符串是否由子母或者数字组成
+
+#
+# #判断数字
+# num1= b'4'
+# num2= u'4'  #python3 默认就是 unicod
+# num3= '四'
+# num4= '肆'
+# num5= 'IV'
+# print(msg.isdigit())    #只能判断 bytes 和 unicode 类型的。
+# print(msg.isdecimal())  #只能判断 bytes 类型的
+# print(msg.isnumeric())  #能判断2 3 4 5四种类型的
+# 这三个不能判断浮点类型的数据
+
+# 例子：
+# age = 10
+# inp=input('you age: ')
+# else:
+#     if inp.isdigit():
+#         inp=int(inp)
+#         if age == inp:
+#             print('OK')
+#         else:
+#             print('error')
+
+
+# msg = '    '
+# print(msg.isspace())  #判断字符串是否由空格组成，而不是判断为空。
+# msg = 'if'
+# print(msg.isidentifier())
+
+# 总结：字符串
+# 存的是一个值，有序 ，不可变，
+
+
+# 练习
+# name = ' aleX'
+# print(name.strip())
+# print(name.startswith('al'))
+# print(name.endswith('X'))
+# print(name.replace('l','p',1))
+# print(name.split('l'))
+# print(name.upper())
+# print(name.lower())
+# print(name[1])
+# print(name[2])
+# print(name[-2:])
+# print(name.index('e'))
+# print(name[0:-1])
+
+
+
+################列表#############
+# 列表   取数据时要提前知道有几项，不方便。
+# stu=['egon','alex','wupeiqi',5]
 # print(stu)
 # print(stu[2])
 # stu_info=[['egon',18,['play',]],['alex',19,['play','sleep']]]
@@ -48,27 +218,282 @@
 # print(stu_info[0])
 # print(stu_info[1][2])
 # print(stu_info[1][2][1])
+# 操作:
+# print(stu[1])
+# stu.append('ela')
+# print(stu)
+# stu[0]='ALEX'
+# print(stu)
+
+# 切片： 步长
+# print(stu[0:3:2])
+
+# 长度
+# print(len(stu))
+
+# 成员判断:
+# print('alex' in stu)
+# print('ales' not in  stu)
+# print(5 in stu)
+#
+
+# 增删
+# stu.append(7)
+# print(stu)
+#
+# del stu[0]
+# print(stu)
+#
+# stu.remove('wupeiqi')  #remove 只是单纯的是难处，不会返回删除的值，按值来删除，
+# print(stu)
+#
+# res=stu.pop(1)  #是按照索引来删除，默认是从末尾删除。
+# print(stu,res)
+
+# 其他操作:
+# stu=['alex','wupeiqi','egon','linhaifeng','ela']
+# stu.insert(0,'fls')
+# print(stu)
+
+# stu.extend(['a','b','c'])  # 两个列表的合并，只能在末尾加
+# print(stu)
+
+# print(stu.count('alex'))
+
+# print(stu.clear())
+
+# l=stu.copy()
+# print(l)
+
+# stu.reverse()
+# print(stu)
+#
+# l=[1,3,6,4,7,3,5]
+# l.(reverse=True)  #原有的列表将会修改，不存在了。
+# print(l)
+
+# 练习：
+# data=['alex',49,[1900,3,18]]
+# name=data[0]
+# age=data[0]
+# year=data[2][0]
+# month=data[2][1]
+# day=data[2][2]
+# print(name,age,year,month,day)
 
 
-#字典   可以实现key-value的形式进行取值。
-#     'name':'egon',
-#     'hobbies':['play','sleep'],
-#     'age':18,
-#     'sex':'male',
-#     'company':{
-#         'name':'oldboy',
-#         'type':'edu',
-#         'emp_num':40,
-#     }
+# sort 的两种效果
+# l=[
+#     {'name':'alex','age':84},
+#     {'name':'oldboy','age':73},
+#     {'name':'egon','age':18},
+# ]
+#
+# l.sort(key=lambda  l:l['age'])
+# print(l)
+#
+# l=[('dave', 'B', 23), ('jane', 'B', 2), ('john', 'A', 18)]
+# l.sort(key=lambda l:l[2])
+# print(l)
+#
+# operator 函数进行排序，这个需要导包，以后再说，
+
+
+# 使用列表模拟 队列和堆栈的效果
+
+# l=[]
+# l.append('first')
+# l.append('second')
+# l.append('third')
+#
+# print(l.pop(0))
+# print(l.pop(0))
+# print(l.pop(0))
+
+# l=[]
+# l.insert(0,'fist')
+# l.insert(0,'second')
+# l.insert(0,'third')
+# print(l)
+#
+# print(l.pop(0))
+# print(l.pop(0))
+# print(l.pop(0))
+
+
+#######元组类型###########
+# 元组和列表相似，但是不能被修改，用小括号标示。
+# tup1 = ('f','l','s')
+# tup2 = (1,2,3)
+# tup3 = "a","b","c","d"
+#
+# 创建空元组
+# tup = ()
+# #元组中只包含一个元素时，需要在元素后面加逗号
+# tup = (52,)
+# #和列表相似，使用下标引用，可以进行截取，组合。
+# #访问:顾头不顾尾
+# print(tup1[1])
+# print(tup3[1:2])
+# print(tup1[1:])
+# #修改元组：拼接和相乘：
+# print(tup1+tup2)
+# print(3*tup2)
+# #del tup1  手动删除元组
+# #元组运算符
+# print(len((1,2,3)))
+# print(2 in (1,2,3))
+# #元组的内置函数
+# print(min((1,2,3))
+# print(max((1,2,3)))
+# #元组和列表的转换
+# print(tuple("nihao"))
+# print(tuple(list("wobuhao")))
+
+# 其他操作：
+# tup=('a','b','a','d','e')
+# print(tup.count('a'))
+# print(tup.index('b'))
+
+# 练习：
+# dic = {
+#     'apple': 10,
+#     'tesla': 10000,
+#     'mac': 3000,
+#     'lenovo': 30000,
+#     'chicken': 10
 # }
 #
+# good = []
+# while True:
+#     for key, item in dic.items():
+#         print('name:{name} price:{price}'.format(name=key, price=item))
+#     choice = input('商品名称：').strip()
+#     if choice == 'quit':
+#         break
+#     if not choice or choice not in dic:
+#         print('请按下面的输入：')
+#         continue
+#     count = input('购买个数： ').strip()
+#     if not count.isdigit():
+#         print('请输入数字：')
+#         continue
+#     else:
+#         good.append((choice, dic[choice], int(count)))
+#
+# print(good)
+# length=len(good)
+# zong=0
+# print(length)
+# y=0
+# for x in good:
+#     while y < length:
+#         print(y)
+#         price=int(good[y][1])
+#         geshu=int(good[y][2])
+#         zong+=price*geshu
+#         y += 1
+#         print(zong)
+
+# print('总价钱为：%d' %(zong))
+#
+#
+
+# #############字典###############
+# 可以实现key-value的形式进行取值，取值速度快
+# key 必须是不可变类型，value 可以是任意类型，
+# 列表、词典这种都是可变类型，所以都是不能作为 key 的。元组就可以作为 key
+# user = {
+#     'name': 'egon',
+#     'hobbies': ['play', 'sleep'],
+#     'age': 18,
+#     'sex': 'male',
+#     'company': {
+#         'name': 'oldboy',
+#         'type': 'edu',
+#         'emp_num': 40,
+#     }
+# }
+
 # print(info['company']['name'])
+# key  value   item
+# print(user.pop('na','None'))
+# print(user.keys(),user.values())
+# for item in user.items():
+#     print(item)
+
+# print(user.get('name'))
+# print(user.popitem()) # 弹出 key 和 value，pop 只弹出 key
+
+# 两种赋值方式
+# 1。链式赋值
+# x = y = z = 10
+# 交换两个变量的值：
+# m = 10
+# n = 20
+
+# tmp=n
+# n=m
+# m=tmp
+# print(m,n,tmp)
+
+# python 的语法：
+# m,n=n,m
+# print(m,n)
+# 2.从一个数据类型中解压出我们想要的值
+# t=(21,54,4.5,35,56)
 #
-# print('''
-# ''')
+# x,y,z,a,b=t
+# print(x,y,z,a,b)
 #
 #
-#这是列表和字典的同时使用。
+# x,_,_z,_,b=t
+# print(_)
+#
+# x,*_,b=t
+# print(b)
+#
+# x,*_='hello'
+# print(x)
+#
+# x,y,z={'a':1,'b':2,'c':3}
+# print(x,y,z)
+#
+# 直接取出元组的k v 值
+# for k,v in user.items():
+#     print(k,v)
+#
+# user_1={'name':'alex','height':180}
+# user.update(user_1)
+# print(user)
+
+# dic1={}.fromkeys(['name','age','hobboes'],None)
+# print(dic1)
+
+
+
+# user = {
+#     'name': 'egon',
+#     'hobbies': ['play', 'sleep'],
+#     'age': 18,
+#     'sex': 'male',
+#     'company': {
+#         'name': 'oldboy',
+#         'type': 'edu',
+#         'emp_num': 40,
+#     }
+# }
+
+# 如果没有就直接插入，如果有就返回原来的值
+# 用法:
+# print(user.setdefault('name','egon'))
+# user.setdefault('hobbies',[]).append('read')
+# print(user.setdefault('hobbies',[]))
+# user.setdefault('hobbies').append('read')
+# print(user)
+
+
+# 这是列表和字典的同时使用。
 # stu=[
 #     {'name':'alex','age':18,'hobbies':['paly','sleep']},
 #     {'name':'egon','age':19,'hobbies':['read','walk']},
@@ -84,65 +509,140 @@
 # # age = input('youe age: ')
 #
 # print('your name is %s and your age is %s'%(name,age))
-
-#元组类型：
-#元组和列表相似，但是不能被修改，用小括号标示。
-tup1 = ('f','l','s')
-tup2 = (1,2,3)
-tup3 = "a","b","c","d"
-
-#创建空元组
-tup = ()
-#元组中只包含一个元素时，需要在元素后面加逗号
-tup = (52,)
-#和列表相似，使用下标引用，可以进行截取，组合。
-#访问:顾头不顾尾
-print(tup1[1])
-print(tup3[1:2])
-print(tup1[1:])
-#修改元组：拼接和相乘：
-print(tup1+tup2)
-print(3*tup2)
-#del tup1  手动删除元组
-#元组运算符
-print(len((1,2,3)))
-print(2 in (1,2,3))
-#元组的内置函数
-print(min((1,2,3)))
-print(max((1,2,3)))
-#元组和列表的转换
-print(tuple("nihao"))
-print(tuple(list("wobuhao")))
-
-
-#布尔类型：
-#not这个特殊
+#
+# 练习：
+# 1.
+# a={'k1':[],'k2':[]}
+# c=[11,22,33,44,55,66,77,88,99,90]
+# for i in c:
+#     if i > 66:
+#         a['k1'].append(i)
+#     else:
+#         a['k2'].append(i)
+# print(a)
+2.
+# a='hello alex alex say hello sb sb'
+# l=a.split()
+# print(l)
+# dic={}
+# for item in l:
+#     if item in dic:
+#         dic[item]+=1
+#     else:
+#         dic[item]=1
+# print(dic)
+# print(dic['hello'])
 
 #
-#打印格式：
+#############集合烈性##############
+# 作用：去重 ，关系运算：交叉并补
+# 优先操作。
+# 每个元素必须是不可变类型，不能重复（自动去重），无序的,
+# 定义
+# s={1,2,'a','a','a'} #s=set{1,2,'a'}
+# print(type(s))
+# print(s)
+# print(len(s))
+# print('a' in s)
+# for item in s:
+#     print(item)
+# s1 = {1, 2, 4}
+# s2 = {4, 6, 5}
+# print(s1 | s2)
+# print(s1.union(s2))
+# print(s1 & s2)
+# print(s1.intersection(s2))
+# print(s1 - s2 )
+# print(s1.difference(s2))
+# print(s1 ^ s2)
+# print(s1.symmetric_difference(s2))
+
+
+# >= s1 包含 s2
+# print(s1.issuperset(s2))
+# <=  s2 包含 s1
+# print(s2.issubset(s1))
+# ==  s1  等于 s2
+
+# 常用操作，
+# 直接删除，不存在则报错
+# s1.pop() # 有返回值 随机删除
+# s1.remove(1)#无返回值
+# print(s1)
+# 直接删，不存在则不会报错
+# s1.discard(1)
+# print(s1)
+##
+# s1.add(3)
+# print(s1)
+
+# p rint(s1.isdisjoint(s2)) # 没有交集则返回 true
+
+# 去重：
+# l=[1,1,3,4,5,6,6]  # 不保证顺序了，和列表的顺序一样的
+# s=list(set(l))
+# print(s)
+
+
+# 集合和元组一起做去重，  元组是不可变类型
+# l = [
+#     {'name': 'egon', 'age': 18, 'sex': 'male'},
+#     {'name': 'alex', 'age': 73, 'sex': 'male'},
+#     {'name': 'egon', 'age': 20, 'sex': 'female'},
+#     {'name': 'egon', 'age': 18, 'sex': 'male'},
+#     {'name': 'egon', 'age': 18, 'sex': 'male'},
+# ]
+# s = set()
+# l2 = []
+# for item in l:
+#     val = (item['name'], item['age'], item['sex'])
+#     if val not in s:
+#         s.add(val)
+#         l2.append(item)
+# print(l)
+#
+
+
+# 练习：
+# pythons={'alex','egon','yuanhao','wupeiqi','gangdan','biubiu'}
+# linuxs={'wupeiqi','oldboy','gangdan'}
+#
+# print(pythons & linuxs)
+# print(pythons | linuxs)
+# print(pythons - linuxs)
+# print(pythons ^ linuxs)
+
+
+#
+#
+# 布尔类型：
+# not这个特殊
+#
+#
+# 打印格式：
 # %s %d 有区别，%s 可以接受任意字符串和数字，而%d只能接受数字，使用的时候要注意默认的规则，比如input都是字符串。
 # name=input('name: ')
 # age=input('age: ')
 # sex=input('sex: ')
 # job=input('job: ')
-# print('------info of egon------')
+# print('info if egon'.center(20,'-'))
 # print('Name : %s' %name)
 # print('Age  : %s' %age)
 # print('Sex  : %s' %sex)
 # print('job  : %s' %job)
-# print('--------- end-----------')
-
-
-#运算
-#/ // %  除法的三个特性。
-
-
-
-#boolean 类型的值  not 就是取反的意思，
-
-
-#if 判断的使用  if  elif  else  注意   ：
-
+# print('end'.center(20,'-'))
+#
+#
+# 运算
+# / // %  除法的三个特性。
+#
+#
+#
+# boolean 类型的值  not 就是取反的意思，
+#
+#
+# if 判断的使用  if  elif  else  注意   ：
+#
 # name=input('name: ')
 # password=input('password :')
 #
@@ -150,9 +650,9 @@ print(tuple(list("wobuhao")))
 #     print('egon login success')
 # else:
 #     print('用户名或者密码错误')
-
-
-
+#
+#
+#
 # '''
 # egon --->超级管理员
 # tom --->普通管理员
@@ -169,8 +669,8 @@ print(tuple(list("wobuhao")))
 #     print('业务主管')
 # else:
 #     print('普通用户')
-
-#多种情况判断下如何简写。
+#
+# 多种情况判断下如何简写。
 # today=input('>>:')
 # if today in ['saturday','sunday']:
 #     print('出去浪')
@@ -186,9 +686,9 @@ print(tuple(list("wobuhao")))
 #     Saturday
 #     Sunday
 #     ''')
-
-
-#while循环的   通常和if联合使用
+#
+#
+# while循环的   通常和if联合使用
 #
 # count=0
 # while count <=10:
@@ -208,12 +708,12 @@ print(tuple(list("wobuhao")))
 #         print('loop',count)
 #
 #     count+=1
-
-#break  退出本层循环
-#continue 退出本次循环
+#
+# break  退出本层循环
+# continue 退出本次循环
 # name='egon'
 # password='123'
-
+#
 # while True:
 #     inp_name=input('用户名: ')
 #     inp_pwd=input('密码: ')
@@ -229,10 +729,10 @@ print(tuple(list("wobuhao")))
 #         print('用户名或者密码错误')
 #         continue
 #     break
-
 #
-
-#加tag 的话这样就不不用写多个 break，一个 tag就将全部的循环条件都置为 False，这样所有的循环就结束掉了
+#
+#
+# 加tag 的话这样就不不用写多个 break，一个 tag就将全部的循环条件都置为 False，这样所有的循环就结束掉了
 # tag=True
 # while tag:
 #     inp_name=input('name: ')
@@ -248,7 +748,7 @@ print(tuple(list("wobuhao")))
 #     else:
 #         print('用户名或者密码错误')
 #     break
-
+#
 # while True:
 #     print('123')
 #     break
@@ -259,10 +759,10 @@ print(tuple(list("wobuhao")))
 #     print('123')
 #     continue
 #     print('456')
-
-
-
-#while +else ： 表示while循环被正常执行完，中间没有被break中断，就会执行else后面的内容。
+#
+#
+#
+# while +else ： 表示while循环被正常执行完，中间没有被break中断，就会执行else后面的内容。
 # count=0
 #
 # while count <= 5:
@@ -273,12 +773,12 @@ print(tuple(list("wobuhao")))
 #
 # else:
 #     print('上面的whle正常的执行完了')
-
-
-
-
-#八个 练习题：
-
+#
+#
+#
+#
+# 八个 练习题：
+#
 # count=1
 # while count<=10:
 #     if count == 7:
@@ -294,7 +794,7 @@ print(tuple(list("wobuhao")))
 #     sum+=count
 #     count+=1
 # print(sum)
-
+#
 # count=1
 # while count <= 100:
 #     if count%2 != 0:
@@ -317,8 +817,8 @@ print(tuple(list("wobuhao")))
 #     count+=1
 #
 # print(sum)
-
-
+#
+#
 #
 # count=0
 # while count < 3:
@@ -330,8 +830,8 @@ print(tuple(list("wobuhao")))
 #     else:
 #         print('fsild')
 #         count+=1
-
-
+#
+#
 # age=65
 # count=0
 # while count < 3:
@@ -342,8 +842,8 @@ print(tuple(list("wobuhao")))
 #     count += 1
 #     # else:
 #     #     print('猜错了，重新猜')
-
-
+#
+#
 # age=65
 # count=0
 # while True:
@@ -359,68 +859,52 @@ print(tuple(list("wobuhao")))
 #         print('你猜对了')
 #         break
 #     count+=1
-
-#for循环，这个比较简单。
+#
+# for循环，这个比较简单。
 #
 # for i in range(1,10):
 #     for j in range(1,i+1):
 #         print('%s * %s=%s is :' %(i,j,i*j),end=' ')
 #     print()
-
-
-#作业:登陆接口
-# user=['test','test1','test2','test3']
-# password=['test','1test','2test','3test']
-# count=0
-# while True:
-#     if count > 2:
-#         print('''
-#   你操作太频繁,请稍后再试!
-#         ''')
-#         break
-#     name = input('your name: ')
-#     pwd = input('your password: ')
-#     if  not name in user:
-#         print('用户名不存在')
-#         continue
-#     id=user.index(name)
-#     if  pwd == password[id]:
-#         print('欢迎登陆！')
-#         break
-#     else:
-#         print('用户名或者密码错误！请重试')
-#         count+=1
-# #进阶：
-# user=['test','test1','test2','test3']
-# password=['test','1test','2test','3test']
-# count=0
-# while True:
-#     name = input('your name: ')
-#     pwd = input('your password: ')
-#     with open('denylogin.txt', 'r')  as file:
-#         denylist = file.read()
-#         if name in denylist:
-#             print('你的账户已经锁定，不能登陆，请联系管理员！')
-#             break
-#     if  not name in user:
-#         print('用户名不存在')
-#         continue
-#     id=user.index(name)
-#     if  pwd == password[id]:
-#         print('欢迎登陆！')
-#         break
-#     else:
-#         print('用户名或者密码错误！请重试')
-#         count+=1
-#     if count > 2:
-#         print('''你操作太频繁,账户已经锁定''')
-#         with open('denylogin.txt','a') as  file :
-#             file.write('%s' %name+'\n')
-#             break
-
-
-
+#
 # user=['test','test1','test2','test3']
 # password=['test','1test','2test','3test']
 # print(user.index('test3'))
 #
+
+
+
+
+###########字符编码##########
+# 直接参见资料
+
+
+
+
+
+
+
+
+
+good = {
+    '1':{'apple': 100},
+    '2':{'tesla': 100},
+    '3':{'mac': 100},
+    '4':{'lenovo': 100},
+    '5':{'chicken': 100},
+}
+
+
+
+print(list(good['1'])[0])
+# print(mc)
+print(good['1']['apple'])
+# x={'a':'10'}
+# b=(list(x))
+# print(b)
+# c=(b[0])
+# print(list(x)[0])
+# print(x[list(x)])
+
+
+
