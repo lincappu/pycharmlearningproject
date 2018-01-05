fff6c7354f1be6021af4b168b20c6b5672b3476d
lincappu <lincappu@163.com>
2018-01-05 08:41:41 +0800

    feat(module)   包的概念与使用

---
+++ day3/module/05 packages.py
@@ -0,0 +1,37 @@
+# !/usr/bin/env python3
+# _*_coding:utf-8_*_
+
+
+# 包的概念：
+
+# 包的本质是个模块，不是执行用的，而是导入使用，方便开发者编程架构。
+# 包的下面有__init__.py文件。
+
+# 导入包实际就是在导入包下面的 init.py的文件
+
+# 环境变量是以执行文件为准。
+
+# .的左边必须是包
+
+
+
+import module
+
+
+# print(module.x)
+# print(module.y)
+
+
+# module.m1
+# module.m1.f1()
+
+
+
+# module.f1()
+
+
+
+# module.test.m3.f3()
+
+
+# module.f3()
