8800b92d7c02ce7bb73387bf0d70e61fa21c38e4
lincappu <lincappu@163.com>
2018-01-11 22:54:48 +0800

    feat(module)  shlex 模块

--- day4/Common module/12 shlex module.py
+++ day4/Common module/12 shlex module.py
@@ -5,13 +5,18 @@
 import  shlex
 
 # 标准形式
-s='ls -l | grep pdf'
-print(shlex.split(s))  # ['ls', '-l', '|', 'grep', 'pdf']
+# s='ls -l | grep pdf'
+# print(shlex.split(s))  # ['ls', '-l', '|', 'grep', 'pdf']
 
 # 牵扯到转义字符
-s='ls -l  | grep \"wo shi shui\"'
-print(shlex.split(s))  # [['ls', '-l', '|', 'grep', 'wo shi shui']
-print(shlex.split(s))  # ['ls', '-l', '|', 'grep', 'wo shi shui']
+# s='ls -l  | grep \"wo shi shui\"'
+# print(shlex.split(s))  # [['ls', '-l', '|', 'grep', 'wo shi shui']
+# print(shlex.split(s))  # ['ls', '-l', '|', 'grep', 'wo shi shui']
 
 
+s='ls -l | grep pdf'
 
+obj=shlex.shlex(s)
+
+print(obj.get_token())
+print(obj.get_token())
